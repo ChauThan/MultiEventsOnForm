e289bc635e8559dd3c8621519cb7a0ee18f12eb0
Than Duc Hoang Chau <Chau.ThanDucHoang@harveynash.vn>
2015-04-21 23:24:52 +0700

    demo

---
+++ MultiEventsOnForm/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiEventsOnForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to kick-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        [HttpPost]
+        [MultiButton(Name = "action", Argument = "Hello")]
+        public ActionResult Hello(string name)
+        {
+            ViewBag.Message = "Hello " + name;
+
+            return View("Index");
+        }
+
+        [HttpPost]
+        [MultiButton(Name = "action", Argument = "Greeting")]
+        public ActionResult Greeting(string name)
+        {
+            ViewBag.Message = "Greeting " + name;
+
+            return View("Index");
+        }
+    }
+}
